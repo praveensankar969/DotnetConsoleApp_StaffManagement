778d23d8f73bd73b884990506b40b44854555e33
praveensankar969 <sankar_praveen@hotmail.com>
2021-05-14 12:14:52 +0530

    exe checked

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,9 @@
+using System;
+
 namespace helloworld{ 
     public class Startup{ 
         public static void Main(string[] args){ 
+           
             InitialLandingPage logon = new InitialLandingPage();
             logon.LogonMain();
         }
